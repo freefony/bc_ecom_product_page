d210a60b998a12a279e61e5f90cd22fe1cc33317
Femi Oni <freefony@gmail.com>
2021-03-02 00:17:53 +0100

    feat: initial setup

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # BC Ecom Product Page
+
+## Components
+- *ProductPageLayout:*  
+- *ProductDisplay*
+- *Thumbnails*
+## Setup
+
+To get the app running on a local machine run the following commands:
+
+- `git clone git@github.com:freefony/bc_ecom_product_page.git`
+- `cd bc_ecom_product_page`
+- `yarn`
+- `yarn start`
+
+These should install the application, install the its dependencies and run the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+
+run  `yarn test` to launch the test runner in the interactive watch mode.\
+
+
+run `yarn build` to builds the app for production to the `build` folder.\
+
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { ProductGellery } from './Gallery';
+import { ProductInfo } from './product/ProductInfo'
+import * as api from './api'
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [product, setProduct] = useState()
+  const productId = window.location.pathname.split('/').pop()
+
+  useEffect(() => {
+    api.getProduct(productId)
+    .then(setProduct)
+    .catch(err => console.log(err)) //Also tell users the product was not found
+  }, [productId])
+
+  if (!product) return <div>Loading...</div>
+
+  return (
+    <div className='App'>
+      <div id='page_layout' className='gap-2'>
+        <ProductGellery images={product.product_images} />
+        <ProductInfo product={product} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
